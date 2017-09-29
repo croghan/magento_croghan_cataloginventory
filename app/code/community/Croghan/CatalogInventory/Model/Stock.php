049e31f2f0a9194fa99238c2b3943acaa06c4116
Michael Croghan <mike@digitalkeg.com>
2017-09-28 20:30:38 -0700

    issue #n/a; added rewrites for cataloginventory models stock and stock_item

---
+++ app/code/community/Croghan/CatalogInventory/Model/Stock.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Stock model
+ *
+ * @method Croghan_CatalogInventory_Model_Resource_Stock _getResource()
+ * @method Croghan_CatalogInventory_Model_Resource_Stock getResource()
+ * @method string getStockName()
+ * @method Croghan_CatalogInventory_Model_Stock setStockName(string $value)
+ *
+ * @category    Croghan
+ * @package     Croghan_CatalogInventory
+ * @author      Michael Croghan <magento@digitalkeg.com>
+ */
+class Croghan_CatalogInventory_Model_Stock extends Mage_CatalogInventory_Model_Stock
+{
+    // tbd //
+}
---
+++ app/code/community/Croghan/CatalogInventory/Model/Stock/Item.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Catalog Inventory Stock Model
+ *
+ * @method Croghan_CatalogInventory_Model_Resource_Stock_Item _getResource()
+ * @method Croghan_CatalogInventory_Model_Resource_Stock_Item getResource()
+ * @method Croghan_CatalogInventory_Model_Stock_Item setProductId(int $value)
+ * @method Croghan_CatalogInventory_Model_Stock_Item setStockId(int $value)
+ * @method float getQty()
+ * @method Croghan_CatalogInventory_Model_Stock_Item setQty(float $value)
+ * @method Croghan_CatalogInventory_Model_Stock_Item setMinQty(float $value)
+ * @method int getUseConfigMinQty()
+ * @method Croghan_CatalogInventory_Model_Stock_Item setUseConfigMinQty(int $value)
+ * @method int getIsQtyDecimal()
+ * @method Croghan_CatalogInventory_Model_Stock_Item setIsQtyDecimal(int $value)
+ * @method Croghan_CatalogInventory_Model_Stock_Item setBackorders(int $value)
+ * @method int getUseConfigBackorders()
+ * @method Croghan_CatalogInventory_Model_Stock_Item setUseConfigBackorders(int $value)
+ * @method Croghan_CatalogInventory_Model_Stock_Item setMinSaleQty(float $value)
+ * @method int getUseConfigMinSaleQty()
+ * @method Croghan_CatalogInventory_Model_Stock_Item setUseConfigMinSaleQty(int $value)
+ * @method Croghan_CatalogInventory_Model_Stock_Item setMaxSaleQty(float $value)
+ * @method int getUseConfigMaxSaleQty()
+ * @method Croghan_CatalogInventory_Model_Stock_Item setUseConfigMaxSaleQty(int $value)
+ * @method Croghan_CatalogInventory_Model_Stock_Item setIsInStock(int $value)
+ * @method string getLowStockDate()
+ * @method Croghan_CatalogInventory_Model_Stock_Item setLowStockDate(string $value)
+ * @method Croghan_CatalogInventory_Model_Stock_Item setNotifyStockQty(float $value)
+ * @method int getUseConfigNotifyStockQty()
+ * @method Croghan_CatalogInventory_Model_Stock_Item setUseConfigNotifyStockQty(int $value)
+ * @method Croghan_CatalogInventory_Model_Stock_Item setManageStock(int $value)
+ * @method int getUseConfigManageStock()
+ * @method Croghan_CatalogInventory_Model_Stock_Item setUseConfigManageStock(int $value)
+ * @method int getStockStatusChangedAutomatically()
+ * @method Croghan_CatalogInventory_Model_Stock_Item setStockStatusChangedAutomatically(int $value)
+ * @method int getUseConfigQtyIncrements()
+ * @method Croghan_CatalogInventory_Model_Stock_Item setUseConfigQtyIncrements(int $value)
+ * @method Croghan_CatalogInventory_Model_Stock_Item setQtyIncrements(float $value)
+ * @method int getUseConfigEnableQtyIncrements()
+ * @method Croghan_CatalogInventory_Model_Stock_Item setUseConfigEnableQtyIncrements(int $value)
+ * @method Croghan_CatalogInventory_Model_Stock_Item setEnableQtyIncrements(int $value)
+ *
+ * @category    Croghan
+ * @package     Croghan_CatalogInventory
+ * @author      Michael Croghan <magento@digitalkeg.com>
+ */
+class Croghan_CatalogInventory_Model_Stock_Item extends Mage_CatalogInventory_Model_Stock_Item
+{
+    // tbd //
+}
